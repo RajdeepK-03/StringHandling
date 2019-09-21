3a1f6393c1dcaa8a477b09b9687bf7219bf73526
macstudent <chahal.rajdeep03@gmail.com>
2019-09-20 20:44:48 -0400

    date time

--- src/main/java/com/lambton/StringHandling.java
+++ src/main/java/com/lambton/StringHandling.java
@@ -1,7 +1,32 @@
 package com.lambton;
 
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+
 public class StringHandling {
     public static void main(String[] args) {
+
+        String pattern = "###,###,##.00";
+        DecimalFormat decimalFormat = new DecimalFormat();
+        decimalFormat.applyPattern(pattern);
+        int value = 12345678;
+        String output = decimalFormat.format(value);
+        System.out.println(value + " " + pattern + " " + output);
+
+        // date and time
+        System.out.println(LocalDateTime.now());
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println(dtf.format(now));
+
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        Date date = new Date();
+        System.out.println(formatter.format(date));
+
         String s1 = "Hello";
         String s2 = "Hello";
         String s3 = null;
@@ -44,16 +69,20 @@ public class StringHandling {
         char c[] = collegeName.toCharArray();
         char rev[] = new char[c.length];
         int cnt = 0;
-
         for(int i = c.length-1; i>=0; i--)
         {
             rev[cnt++] = c[i];
         }
         String reverseString = new String(rev);
         System.out.println(" Reverse String =" + reverseString);
+        // Reverse only even position words
+        // input: lambton college in toronto
+        //Output: lambton egelloc in otnoroT
+
 
 
     }
+
 
 
 }
